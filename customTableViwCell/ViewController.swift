b8c964dc5c4c8daca4e7793f7b16d961d5c1e9c1
Kate Klavina <kate.klavina97@gmail.com>
2019-11-14 12:45:17 +0200

    Initial Commit

---
+++ customTableViwCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customTableViwCell
+//
+//  Created by Dace Laizane on 14/11/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
